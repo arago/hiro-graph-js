0b2bd3932818b15b11a995ed2f7bfa6344fbd8a5
Chris Walker <cwalker@arago.de>
2017-06-15 15:41:00 +0100

    Fix connect bug

--- packages/hiro-graph-orm/src/vertex/graph.js
+++ packages/hiro-graph-orm/src/vertex/graph.js
@@ -95,7 +95,7 @@ export default class GraphVertex extends Vertex {
      */
     connect(relation, vertexOrId) {
         return this._db
-            .connect(relation, vertexOrId)
+            .connect(relation, this._id, vertexOrId)
             .then(refetchRelationData(this, relation));
     }
 
