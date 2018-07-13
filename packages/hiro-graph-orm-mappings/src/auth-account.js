8ef3f961db00fced85452e6008857b97601790e2
Mikhail Osher <mosher@klika-tech.com>
2018-07-13 17:21:21 +0300

    HD-344: fix formatting

--- packages/hiro-graph-orm-mappings/src/auth-account.js
+++ packages/hiro-graph-orm-mappings/src/auth-account.js
@@ -1,14 +1,14 @@
 export default {
-  name: "AuthAccount",
-  ogit: "ogit/Auth/Account",
-  required: {
-    name: "ogit/name",
-    status: "ogit/status",
-  },
-  optional: {
-    roles: {
-      src: "/roles",
-      type: "list",
+    name: "AuthAccount",
+    ogit: "ogit/Auth/Account",
+    required: {
+        name: "ogit/name",
+        status: "ogit/status"
     },
-  },
+    optional: {
+        roles: {
+            src: "/roles",
+            type: "list"
+        }
+    }
 };
