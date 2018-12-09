de8e6f0c193d89606f863f94959abbf2cc3b4121
Dominic Cassidy <dcassidy@arago.de>
2018-12-09 10:56:26 +0000

    Correct index export format for mapping

--- packages/hiro-graph-orm-mappings/src/helper.ts
+++ packages/hiro-graph-orm-mappings/src/helper.ts
@@ -1,34 +1,51 @@
 export const getName = (value: string) => {
-  const split = value.replace(/-/g, "").split("/");
-  if (split.length === 2) {
-    return split[1];
-  }
+    const split = value.replace(/-/g, "").split("/");
+    if (split.length === 2) {
+        return split[1];
+    }
 
-  return split[split.length - 2] + split[split.length - 1];
+    return split[split.length - 2] + split[split.length - 1];
 };
 
 export const createIndex = (output: IOutput) => {
-  let imports = "";
-  let exports = "";
-  const keys = Object.keys(output).sort((a, b) => {
-    if (a === b) {
-      return 0;
-    }
-    if (a > b) {
-      return 1;
-    }
-    return -1;
-  });
-  keys.map((key, i) => {
-    const value = output[key];
-    const fileName = key
-      .toLowerCase()
-      .replace("ogit/", "")
-      .replace(/\//g, "-");
+    let imports = "";
+    let exports = "";
+    let singleExports = "";
+    const keys = Object.keys(output).sort((a, b) => {
+        if (a === b) {
+            return 0;
+        }
+        if (a > b) {
+            return 1;
+        }
+        return -1;
+    });
+    keys.map((key, i) => {
+        const value = output[key];
+        const fileName = key
+            .toLowerCase()
+            .replace("ogit/", "")
+            .replace(/\//g, "-");
 
-    imports += `import ${value.name} from "./${fileName}";\n`;
-    exports += `  ${value.name}${i === keys.length - 1 ? "" : ",\n"}`;
-  });
+        imports += `const ${value.name} = require("./${fileName}");\n`;
+        exports += `    ${value.name}${i === keys.length - 1 ? "" : ",\n"}`;
+        singleExports += `exports.${value.name} = ${value.name};`;
+    });
 
-  return `${imports}\nexport default [\n${exports}\n];\n\nexport {\n${exports}\n};`;
+    return `"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+${imports}
+
+exports.default = [
+${exports}
+];
+
+${singleExports}
+`;
 };
+
+// \nexport default [\n${exports}\n];
