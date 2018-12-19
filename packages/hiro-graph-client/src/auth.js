4ee1bc97d9ee8ab6af0151e8448d85cec21e632d
Dominic Cassidy <dcassidy@arago.de>
2018-12-19 12:20:20 +0000

    Correct auth.organizationTeams() params to match typings

--- packages/hiro-graph-client/src/auth.js
+++ packages/hiro-graph-client/src/auth.js
@@ -1,4 +1,4 @@
 export const auth = {
-    organizationTeams: (fetch, options, { id }) =>
+    organizationTeams: (fetch, options, id) =>
         fetch(`/api/6.1/iam/organization/${id}/teams`, options)
 };
