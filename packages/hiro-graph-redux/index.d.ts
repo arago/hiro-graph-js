8f5610acd2241e61d6bc2744f7b7646e388d9abf
Dominic Cassidy <dcassidy@arago.de>
2019-01-07 12:32:44 +0000

    Initial typings for hiro-graph/redux

---
+++ packages/hiro-graph-redux/index.d.ts
@@ -0,0 +1,48 @@
+export type ITaskShape<T = any> = {
+    error?: Error | false | string;
+    loading: boolean;
+    result: T;
+    start: number;
+};
+
+export const graphReducer: () => void;
+export const createVertexSelector: () => void;
+export const createTaskSelector: () => void;
+export const createAction: () => void;
+export const createTask: <T = any>(
+    action: any,
+    selector: any
+) => {
+    action: (...args: any) => void;
+    selector: (...args: any) => ITaskShape<T>;
+};
+export const createTaskFactory: () => void;
+export const createTaskAction: () => void;
+export const cancelTask: () => void;
+export const resetTask: () => void;
+export const createStoreEnhancer: () => void;
+export const setToken: () => void;
+export const getTaskState: () => void;
+export const getMyId: () => void;
+export const getMyRoles: () => void;
+export const getTokenState: () => void;
+
+interface IWhenTaskOptions<T, R> {
+    pre?: () => R;
+    loading?: () => R;
+    reloading?: () => R;
+    ok?: (res: T) => R;
+    error?: () => R;
+    ignoreReloadingIfOK?: boolean;
+    ignoreReloadingIfError?: boolean;
+}
+
+export const whenTask: <T = any, R = any>(
+    task: ITaskShape<T>,
+    options: IWhenTaskOptions<T, R>
+) => R;
+export const implicitOauth: () => void;
+export const loginTaskSelector: () => void;
+export const doLogin: () => void;
+export const doLogout: () => void;
+export const setOnLogoutHook: () => void;
