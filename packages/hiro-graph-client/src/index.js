77586bd207a3335ee9f9606bc007396ec5497936
Mikhail Osher <mosher@klika-tech.com>
2019-10-10 11:36:34 +0300

    Expose servlets via index.js

--- packages/hiro-graph-client/src/index.js
+++ packages/hiro-graph-client/src/index.js
@@ -2,5 +2,20 @@ import Token, { cannotGetToken } from "./token";
 import Client from "./client";
 import * as Errors from "./errors";
 
+import appsServlet from "./servlets/app";
+import kiServlet from "./servlets/ki";
+import statsServlet from "./servlets/stats";
+import variablesSerlvet from "./servlets/variables";
+
 export default Client;
-export { Token, cannotGetToken, Errors };
+
+export {
+    Token,
+    cannotGetToken,
+    Errors,
+
+    appsServlet,
+    kiServlet,
+    statsServlet,
+    variablesSerlvet,
+};
