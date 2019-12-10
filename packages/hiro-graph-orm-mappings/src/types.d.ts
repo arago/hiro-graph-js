b0ac8c10daf86e48c18f4aeb634d35605d725c23
vteremasov <vteremasov@klika-tech.com>
2019-12-10 11:57:01 +0300

    #170148513: Remove unnecessary type

--- packages/hiro-graph-orm-mappings/src/types.d.ts
+++ packages/hiro-graph-orm-mappings/src/types.d.ts
@@ -8,15 +8,6 @@ interface IMapping {
   [index: string]: string;
 }
 
-// interface IDefinition {
-//   name: string;
-//   ogit: string;
-//   required?: IDefinitionData;
-//   optional?: IDefinitionData;
-//   relations?: IDefinitionData;
-//   [name: string]: string | IDefinitionData | undefined;
-// }
-
 interface IOutput {
   [index: string]: IDefinition;
 }
