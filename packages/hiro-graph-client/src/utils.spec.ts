00f56f12151492007ebce07bd9b4a7f775659cc3
Dominic Cassidy <dcassidy@arago.de>
2020-04-05 16:12:54 +0100

    Create testable utils file

---
+++ packages/hiro-graph-client/src/utils.spec.ts
@@ -0,0 +1,54 @@
+import { extract } from './utils';
+
+type O = Record<string, any>;
+
+test('Extract keys to object', () => {
+  const tests: Array<[O, string[], O]> = [
+    [
+      {
+        a: 1,
+        b: 1,
+        c: 1,
+      },
+      ['b'],
+      {
+        b: 1,
+      },
+    ],
+    [
+      {
+        a: 1,
+        b: 1,
+        c: 1,
+      },
+      ['a', 'b', 'c'],
+      {
+        a: 1,
+        b: 1,
+        c: 1,
+      },
+    ],
+    [
+      {
+        a: 1,
+        b: 1,
+        c: 1,
+      },
+      [],
+      {},
+    ],
+    [
+      {
+        b: 1,
+      },
+      ['a', 'b'],
+      {
+        b: 1,
+      },
+    ],
+  ];
+
+  tests.forEach(([input, arg, expected]) => {
+    expect(extract(input, ...arg)).toEqual(expected);
+  });
+});
