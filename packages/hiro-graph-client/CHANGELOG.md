e4066f0cc6bc445a3078bc8eeaef01d217640c4d
Dominic Cassidy <dcassidy@arago.de>
2020-04-20 21:31:02 +0100

    Clean up

--- packages/hiro-graph-client/CHANGELOG.md
+++ packages/hiro-graph-client/CHANGELOG.md
@@ -1,4 +1,5 @@
 # Changelog
+
 All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
@@ -7,9 +8,19 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ## [Unreleased]
 
 ### Changed
-* Convert to Typescript
-* Re-write Transports (and Eventstream) using RxJS - All functions now return `Observable<T[]>` these can be converted to Promises using `toPromise()`.
-* `InstanceType<Client>.gremlin()` and `InstanceType<Client>.lucene()` now have built in query helpers to reduce code when using these functions.
+
+- Convert to Typescript
+- Add `Endpoint` helper class for easy creation of API urls and queries.
+
+```ts
+const url = new Endpoint('https://graphit.co').api('auth', ['me', 'account'], {
+  profile: true,
+});
+// https://graphit.co/api/auth/6.1/me/account?profile=true
+```
+
+- Re-write Transports (and Eventstream) using RxJS - All functions now return `Observable<T[]>` these can be converted to Promises using `toPromise()`.
+- `InstanceType<Client>.gremlin()` and `InstanceType<Client>.lucene()` now have built in query helpers to reduce code when using these functions.
 
 ```ts
 // Gremlin Query
@@ -48,5 +59,5 @@ const client = new Client(...);
 client.lucene({ '/hello': 'world' });
 ```
 
-* Remove internal un-used code - such as dedup, extra logging, internal emitting of events etc.
-* Remove internal default exports for consistency.
+- Remove internal un-used code - such as dedup, extra logging, internal emitting of events etc.
+- Remove internal default exports for consistency.
--- packages/hiro-graph-client/example.ts
+++ packages/hiro-graph-client/example.ts
@@ -1,57 +1,63 @@
 import { forkJoin } from 'rxjs';
 import { map } from 'rxjs/operators';
 
-import Client, { Servlets } from './src';
+import Client, { Servlets, Endpoint } from './src';
 
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+const url = new Endpoint('https://graphit.co').api('auth', ['me', 'account'], {
+  profile: true,
+});
 
-const token =
-  'TDsmg5Y4mJjUjM3tVeSjlVnpae2knHOKgx483WU2Z847k38OVuFN3cfk2aRVtF17rJejIgXsJZRTiaBcywH7RNsO047JpRKtjQdPhYxRCpf6chk3JWiZcJuWXZFsRyDi';
+console.log(url);
 
-const client = new Client({
-  endpoint: 'https://eu-stagegraph.arago.co',
-  token,
-}).addServlet(Servlets.Variables);
+// process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
-client.variables.add({}).subscribe(console.log);
+// const token =
+//   'TDsmg5Y4mJjUjM3tVeSjlVnpae2knHOKgx483WU2Z847k38OVuFN3cfk2aRVtF17rJejIgXsJZRTiaBcywH7RNsO047JpRKtjQdPhYxRCpf6chk3JWiZcJuWXZFsRyDi';
 
-// const issueFilter = new StreamFilter().and(
-//   `element.ogit/_type = ogit/Automation/AutomationIssue`,
-//   'action = UPDATE',
+// const client = new Client({
+//   endpoint: 'https://eu-stagegraph.arago.co',
+//   token,
+// }).addServlet(Servlets.Variables);
+
+// client.variables.add({}).subscribe(console.log);
+
+// // const issueFilter = new StreamFilter().and(
+// //   `element.ogit/_type = ogit/Automation/AutomationIssue`,
+// //   'action = UPDATE',
+// // );
+
+// // const es = client.eventStream();
+
+// // // @ts-ignore
+// // es.subscribe(console.log);
+
+// // es.register(issueFilter).subscribe(console.log);
+
+// const name$ = client
+//   .lucene(
+//     {
+//       'ogit/_id': 'cjuwixjvq0xfc1q90xqn8jsvf_ck74voksj077n0w46k0cf09xo',
+//     },
+//     {
+//       limit: 1,
+//     },
+//   )
+//   // @ts-ignore
+//   .pipe(map((res) => res && res.map((r) => r['ogit/name'])));
+
+// const members$ = client.gremlin(
+//   'cjuwixjvq0xfc1q90xqn8jsvf_ck74voksj077n0w46k0cf09xo',
+//   (g) =>
+//     g
+//       .inE('ogit/Auth/isMemberOf')
+//       .has('ogit/_out-type', 'ogit/Auth/Account')
+//       .outV()
+//       .count(),
 // );
 
-// const es = client.eventStream();
+// forkJoin({
+//   name: name$,
+//   members: members$,
+// }).subscribe(console.log);
 
-// // @ts-ignore
-// es.subscribe(console.log);
-
-// es.register(issueFilter).subscribe(console.log);
-
-const name$ = client
-  .lucene(
-    {
-      'ogit/_id': 'cjuwixjvq0xfc1q90xqn8jsvf_ck74voksj077n0w46k0cf09xo',
-    },
-    {
-      limit: 1,
-    },
-  )
-  // @ts-ignore
-  .pipe(map((res) => res && res.map((r) => r['ogit/name'])));
-
-const members$ = client.gremlin(
-  'cjuwixjvq0xfc1q90xqn8jsvf_ck74voksj077n0w46k0cf09xo',
-  (g) =>
-    g
-      .inE('ogit/Auth/isMemberOf')
-      .has('ogit/_out-type', 'ogit/Auth/Account')
-      .outV()
-      .count(),
-);
-
-forkJoin({
-  name: name$,
-  members: members$,
-}).subscribe(console.log);
-
-// TS_NODE_COMPILER_OPTIONS='{"module":"commonjs"}' yarn ts-node example.ts
+// // TS_NODE_COMPILER_OPTIONS='{"module":"commonjs"}' yarn ts-node example.ts
