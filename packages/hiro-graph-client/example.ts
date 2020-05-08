d286597cf44a94d47e9f223bb29b0e2afa6c0010
Dominic Cassidy <dcassidy@arago.de>
2020-05-08 20:55:34 +0100

    Try fix commonjs dependencies

--- packages/hiro-graph-client/example.ts
+++ packages/hiro-graph-client/example.ts
@@ -1,7 +1,7 @@
 import { forkJoin } from 'rxjs';
 import { map } from 'rxjs/operators';
 
-import Client, { Servlets, Endpoint } from './src';
+import Client, { Servlets, Endpoint } from './pkg';
 
 const url = new Endpoint('https://graphit.co').api('auth', ['me', 'account'], {
   profile: true,
@@ -9,15 +9,15 @@ const url = new Endpoint('https://graphit.co').api('auth', ['me', 'account'], {
 
 console.log(url);
 
-// process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
-// const token =
-//   'TDsmg5Y4mJjUjM3tVeSjlVnpae2knHOKgx483WU2Z847k38OVuFN3cfk2aRVtF17rJejIgXsJZRTiaBcywH7RNsO047JpRKtjQdPhYxRCpf6chk3JWiZcJuWXZFsRyDi';
+const token =
+  'TDsmg5Y4mJjUjM3tVeSjlVnpae2knHOKgx483WU2Z847k38OVuFN3cfk2aRVtF17rJejIgXsJZRTiaBcywH7RNsO047JpRKtjQdPhYxRCpf6chk3JWiZcJuWXZFsRyDi';
 
-// const client = new Client({
-//   endpoint: 'https://eu-stagegraph.arago.co',
-//   token,
-// }).addServlet(Servlets.Variables);
+const client = new Client({
+  endpoint: 'https://eu-stagegraph.arago.co',
+  token,
+}).addServlet(Servlets.Variables);
 
 // client.variables.add({}).subscribe(console.log);
 
@@ -33,31 +33,31 @@ console.log(url);
 
 // // es.register(issueFilter).subscribe(console.log);
 
-// const name$ = client
-//   .lucene(
-//     {
-//       'ogit/_id': 'cjuwixjvq0xfc1q90xqn8jsvf_ck74voksj077n0w46k0cf09xo',
-//     },
-//     {
-//       limit: 1,
-//     },
-//   )
-//   // @ts-ignore
-//   .pipe(map((res) => res && res.map((r) => r['ogit/name'])));
+const name$ = client
+  .lucene(
+    {
+      'ogit/_id': 'cjuwixjvq0xfc1q90xqn8jsvf_ck74voksj077n0w46k0cf09xo',
+    },
+    {
+      limit: 1,
+    },
+  )
+  // @ts-ignore
+  .pipe(map((res) => res && res.map((r) => r['ogit/name'])));
 
-// const members$ = client.gremlin(
-//   'cjuwixjvq0xfc1q90xqn8jsvf_ck74voksj077n0w46k0cf09xo',
-//   (g) =>
-//     g
-//       .inE('ogit/Auth/isMemberOf')
-//       .has('ogit/_out-type', 'ogit/Auth/Account')
-//       .outV()
-//       .count(),
-// );
+const members$ = client.gremlin(
+  'cjuwixjvq0xfc1q90xqn8jsvf_ck74voksj077n0w46k0cf09xo',
+  (g) =>
+    g
+      .inE('ogit/Auth/isMemberOf')
+      .has('ogit/_out-type', 'ogit/Auth/Account')
+      .outV()
+      .count(),
+);
 
-// forkJoin({
-//   name: name$,
-//   members: members$,
-// }).subscribe(console.log);
+forkJoin({
+  name: name$,
+  members: members$,
+}).subscribe(console.log);
 
-// // TS_NODE_COMPILER_OPTIONS='{"module":"commonjs"}' yarn ts-node example.ts
+// TS_NODE_COMPILER_OPTIONS='{"module":"commonjs"}' yarn ts-node example.ts
