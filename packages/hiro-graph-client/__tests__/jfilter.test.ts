ebb8c9447ed40614f920ee4750eaa735cd9a0e42
Dominic Cassidy <dcassidy@arago.de>
2020-08-27 12:15:43 +0100

    Copy jfilter from desktop-apps

--- packages/hiro-graph-client/__tests__/jfilter.test.ts
+++ packages/hiro-graph-client/__tests__/jfilter.test.ts
@@ -2,25 +2,82 @@ import { JFilter } from '../src/jfilter';
 
 test('JFilter snapshots', () => {
   const tests = [
-    new JFilter().and('a = b', 'd = e'),
-    new JFilter().or('a = b', 'd = e'),
-    new JFilter().and('a = b', new JFilter('d = e')),
-    new JFilter().and('a = b', new JFilter().or('e = f').and('d = c', 'g = f')),
+    JFilter.and(JFilter.equals('a', 'b'), JFilter.equals('d', 'e')),
+    JFilter.or(JFilter.equals('a', 'b'), JFilter.equals('d', 'e')),
+    JFilter.and(JFilter.equals('a', 'b'), JFilter.equals('d', 'e')),
+    JFilter.and(
+      JFilter.equals('a', 'b'),
+      JFilter.or('e=f'),
+      JFilter.and(JFilter.equals('d', 'c'), JFilter.equals('g', 'f')),
+    ),
   ];
 
   tests.map((t) => expect(t.toString()).toMatchSnapshot());
 });
 
 test('JFilter functions', () => {
-  expect(new JFilter().and('a', 'b').toString()).toBe('&(a)(b)');
-  expect(new JFilter().or('a', 'b').toString()).toBe('|(a)(b)');
+  expect(JFilter.equals('a', 'b').toString()).toBe('(a=b)');
+  expect(JFilter.and('a', 'b').toString()).toBe('&(a)(b)');
+  expect(JFilter.or('a', 'b').toString()).toBe('|(a)(b)');
 });
 
 test('JFilter nested', () => {
-  expect(new JFilter().and('a', new JFilter().and('b', 'c')).toString()).toBe(
+  expect(JFilter.and('a', JFilter.and('b', 'c')).toString()).toBe(
     '&(a)(&(b)(c))',
   );
-  expect(new JFilter().or('a', new JFilter().or('b', 'c')).toString()).toBe(
+  expect(JFilter.or('a', JFilter.or('b', 'c')).toString()).toBe(
     '|(a)(|(b)(c))',
   );
+});
+
+test('JFilter transform', () => {
+  expect(
+    JFilter.transform({ body: { 'ogit/_id': 'test' }, type: 'UPDATE' }),
+  ).toEqual({
+    element: { 'ogit/_id': 'test' },
+    action: 'UPDATE',
+  });
+});
+
+test('JFilter test function', () => {
+  expect(
+    JFilter.equals('element.ogit/_id', 'test').test({
+      element: { 'ogit/_id': 'test' },
+      action: 'UPDATE',
+    }),
+  ).toEqual(true);
+
+  expect(
+    JFilter.equals('action', 'UPDATE').test({
+      element: { 'ogit/_id': 'test' },
+      action: 'CREATE',
+    }),
+  ).toEqual(false);
+
+  expect(
+    JFilter.equals('action', '*').test({
+      element: { 'ogit/_id': 'test' },
+      action: 'CREATE',
+    }),
+  ).toEqual(true);
+
+  expect(
+    JFilter.and(
+      JFilter.equals('element.ogit/_id', 'test'),
+      JFilter.equals('action', 'CREATE'),
+    ).test({
+      element: { 'ogit/_id': 'test' },
+      action: 'CREATE',
+    }),
+  ).toEqual(true);
+
+  expect(
+    JFilter.and(
+      JFilter.equals('element.ogit/_id', 'test'),
+      JFilter.equals('action', 'CREATE'),
+    ).test({
+      element: { 'ogit/_id': 'test2' },
+      action: 'CREATE',
+    }),
+  ).toEqual(false);
 });
